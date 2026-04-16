2e5eb4b663b8df090f512d639299e7296fee62e8
Niunek <niuneka7@gmail.com>
2026-04-16 16:40:14 +0200

    kgf

--- smoki.js
+++ smoki.js
@@ -24,11 +24,51 @@ const smoki = [
     zdjecie: "ksismoków/Burzochlast.jpg"
   },
   {
-    nazwa: "Elita",
+    nazwa: "Alfa",
     typ: "Alpha Class",
     atak: "Gigantyczny strumień lodu",
     pokarm: "Duże stworzenia morskie",
     moc: 98,
     zdjecie: "ksismoków/elita.jpg"
   }
+  {
+    nazwa: "Eruptodon",
+    typ: "Boulder Class",
+    atak: "Strzelanie lawą",
+    pokarm: "Skały",
+    moc: 90,
+    zdjecie: "ksismoków/eruptodon.jpg"
+  },
+  {
+    nazwa: "Gronkiel",
+    typ: "Boulder Class",
+    atak: "Strzelanie kulami lawy",
+    pokarm: "Skały",
+    moc: 90,
+    zdjecie: "ksismoków/Gronkiel.jpg"
+  },
+  {
+    nazwa: "Scauldron",
+    typ: "Tidal Class",
+    atak: "Strzelanie wrzącą wodą",
+    pokarm: "Stworzenia morskie",
+    moc: 86,
+    zdjecie: "ksismoków/scauldron.jpg"
+  },
+  {
+    nazwa: "Śmierci pieśń",
+    typ: "Mystery Class",
+    atak: "Hipnotyczny śpiew",
+    pokarm: "Smoki",
+    moc: 89,
+    zdjecie: "ksismoków/śmiercipieśń.jpg"
+  },
+  {
+    nazwa: "Zębiróg zamkogłowy",
+    typ: "Boulder Class",
+    atak: "Potężne szarże",
+    pokarm: "Skały",
+    moc: 87,
+    zdjecie: "ksismoków/zębiróg_zamkogłowy.jpg"
+  },
 ];
