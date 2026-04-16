00f1d73e8ff78f3c20e47dd3ed571a2e17d3bc86
Niunek <niuneka7@gmail.com>
2026-04-16 15:38:59 +0200

    dhfdsh

---
+++ smoki.js
@@ -0,0 +1,18 @@
+const smoki = [
+  {
+    nazwa: "Nocna furia",
+    typ: "Nocna Furia",
+    atak: "Plazmowy strzał",
+    pokarm: "Ryby",
+    moc: 95,
+    zdjecie: "smoki/szczerbatek.jpg"
+  },
+  {
+    nazwa: "Monstrous Nightmare",
+    typ: "Monstrous Nightmare",
+    atak: "Ogień + podpalenie ciała",
+    pokarm: "Mięso",
+    moc: 85,
+    zdjecie: "smoki/hookfang.jpg"
+  }
+];
